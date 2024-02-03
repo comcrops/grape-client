8173284e5b4d2aecf51962b50561d842d9f5064b
s-prechtl <stefan@tague.at>
2024-02-03 18:03:05 +0100

    feat: added pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                icons: [
+                    {
+                        src: "@/assets/grape512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable any"
+                    },
+                ]
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
